a6e5dee5d7a3cc4b66594eb93ab418bccd7aea93
Anwar Imam <anwar.imam412@gmail.com>
2026-04-03 12:18:28 +0530

    mongodb fix

--- server.js
+++ server.js
@@ -1,59 +1,60 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const db = {};
+// ✅ CONNECT MONGODB
+mongoose.connect("mongodb+srv://admin:admin123@cluster0.lphb9i5.mongodb.net/urlshortener")
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
+// ✅ Schema
+const Url = mongoose.model("Url", {
+  originalUrl: String,
+  shortCode: String,
+});
+
+// ✅ Generate code
 const generateCode = () =>
   Math.random().toString(36).substring(2, 7);
 
-const isValidUrl = (url) => {
-  try {
-    new URL(url);
-    return true;
-  } catch {
-    return false;
-  }
-};
-
-app.post("/shorten", (req, res) => {
+// ✅ Shorten API
+app.post("/shorten", async (req, res) => {
   const { url } = req.body;
 
-  if (!isValidUrl(url)) {
-    return res.status(400).json({ error: "Invalid URL" });
-  }
-
-  // reuse existing
-  for (let key in db) {
-    if (db[key] === url) {
-      return res.json({
-        shortUrl: `http://localhost:5000/${key}`,
-      });
-    }
-  }
-
   const code = generateCode();
-  db[code] = url;
+
+  await Url.create({
+    originalUrl: url,
+    shortCode: code,
+  });
 
   res.json({
-    shortUrl: `http://localhost:5000/${code}`,
+    shortUrl: `https://url-shortener-api-dvi0.onrender.com/${code}`,
   });
 });
 
-app.get("/:code", (req, res) => {
-  let url = db[req.params.code];
+// ✅ Redirect
+app.get("/:code", async (req, res) => {
+  const data = await Url.findOne({ shortCode: req.params.code });
 
-  if (url) {
+  if (data) {
+    let url = data.originalUrl;
+
     if (!url.startsWith("http")) {
       url = "https://" + url;
     }
+
     return res.redirect(url);
   }
 
   res.status(404).send("Not found");
 });
 
-app.listen(5000, () => console.log("Server running on 5000"));
+// ✅ Start server
+app.listen(process.env.PORT || 5000, () =>
+  console.log("Server running")
+);
